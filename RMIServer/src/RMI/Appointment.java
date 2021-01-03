da2be1685e99a701f7cae992972973753e1b5392
Meriam Sherif <meriam_kamel@yahoo.com>
2021-01-04 01:11:18 +0200

    Classes
    Only main classes are added and a package for contollers

--- RMIServer/src/RMI/Appointment.java
+++ RMIServer/src/RMI/Appointment.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package rmiserver;
+package RMI;
 
 /**
  *
  * @author meriam
  */
-public class Patient {
-  
+public class Appointment {
+    
 }
