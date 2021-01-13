474b58e8cea626285f578992fb3875027b3a194d
Mariam Mohamed <mariam155654@bue.edu.eg>
2021-01-13 18:17:16 +0200

    Update RMIClient.java

--- RMIClient/src/RMI/RMIClient.java
+++ RMIClient/src/RMI/RMIClient.java
@@ -5,6 +5,9 @@
  */
 package RMI;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 /**
  *
  * @author meriam
@@ -16,6 +19,29 @@ public class RMIClient {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        try {
+            // Connecting to the RMI Registry created on the server
+            Registry registry = LocateRegistry.getRegistry(1099);
+
+            // Search for the stub "calc"
+            FacadeInterface f = (FacadeInterface) registry.lookup("fac");
+
+             //Invoke the remote methods on the remote object
+//           f.SetCourseData("AOOSE ", "10110ch ", " Advanced object oriented SE");
+//           
+//           String CourseInfo =f.getCouseData();
+//           System.out.println("Couse info  : "+CourseInfo);
+            
+           f.SetMedicineData("s ", "s ", " ",2,3);
+           //CourseDTO c= f.getCourse();
+           // System.out.println("cousre name "+c.getName());
+            
+           
+        } catch (Exception ex) {
+            System.out.println("Exception occured");
+        }
+        
+        
     }
     
 }
