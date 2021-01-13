ca6354e8897ac270eabc310b5747394d4afb4a45
Mariam Mohamed <mariam155654@bue.edu.eg>
2021-01-13 12:41:08 +0200

    Update MedicalProfile.java

--- RMIServer/src/RMI/MedicalProfile.java
+++ RMIServer/src/RMI/MedicalProfile.java
@@ -5,10 +5,18 @@
  */
 package RMI;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author meriam
  */
 public class MedicalProfile {
-    
+    private ArrayList<String> drFollowup = new ArrayList();
+    private ArrayList<String> chronicDisease = new ArrayList();
+    private ArrayList<Medicine> medicines = new ArrayList();
+    private String bloodType;
+    private ArrayList<Appointment> pastAppointments = new ArrayList();
+    private ArrayList<Prescription> prescriptions = new ArrayList();
+
 }
