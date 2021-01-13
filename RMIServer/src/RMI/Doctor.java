9ae1c4721edb0c00a1e2822a296a319d6b335598
Mariam Mohamed <mariam155654@bue.edu.eg>
2021-01-13 12:34:41 +0200

    .

--- RMIServer/src/RMI/Doctor.java
+++ RMIServer/src/RMI/Doctor.java
@@ -5,10 +5,21 @@
  */
 package RMI;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author meriam
  */
-public class Doctor {
-    
+public class Doctor extends Account{
+    private int rating;
+    private String levelOfExpertise;
+    private ArrayList<Nurse> nurse = new ArrayList();
+    private ArrayList<String> timeSlots = new ArrayList();
+    private Account account;
+    private ArrayList<OperationRoom> operationRooms = new ArrayList();
+    private DoctorAppointmentFunctionalities doctorAppointmentFunctionalities;
+    private DoctorMedicalFunctionalities doctorMedicalFunctionalities;
+
+
 }
