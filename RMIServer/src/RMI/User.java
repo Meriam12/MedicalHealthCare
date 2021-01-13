391b2ffbe3a67e38ade26d82afbee68994c37abe
Meriam Sherif <meriam_kamel@yahoo.com>
2021-01-13 12:36:02 +0200

    User
    Meriam adds User data

--- RMIServer/src/RMI/User.java
+++ RMIServer/src/RMI/User.java
@@ -11,4 +11,52 @@ package RMI;
  */
 public class User {
     
+    String name;
+    String phonenumber;
+    String birthdate;
+    String email;
+
+    public User() {
+    }
+
+    public User(String name, String phonenumber, String birthdate, String email) {
+        this.name = name;
+        this.phonenumber = phonenumber;
+        this.birthdate = birthdate;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
 }
