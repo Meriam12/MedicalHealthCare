ccafca60acddd32d3eb4576e24cd883f8e77990a
nourahosam <34795230+nourahosam@users.noreply.github.com>
2021-01-13 13:38:51 +0200

    Order Medicine class

--- RMIServer/src/RMI/OrderMedicine.java
+++ RMIServer/src/RMI/OrderMedicine.java
@@ -5,10 +5,64 @@
  */
 package RMI;
 
+import java.util.*;
+
 /**
  *
  * @author meriam
  */
 public class OrderMedicine {
-    
+    private int amountNeeded;
+    private String orderStatus;
+    private ArrayList<Medicine> med;
+    private String patientName;
+    private Payment payment;
+
+    public OrderMedicine(int amountNeeded, String orderStatus, ArrayList<Medicine> med, String patientName, Payment payment) {
+        this.amountNeeded = amountNeeded;
+        this.orderStatus = orderStatus;
+        this.med = med;
+        this.patientName = patientName;
+        this.payment = payment;
+    }
+
+    public int getAmountNeeded() {
+        return amountNeeded;
+    }
+
+    public void setAmountNeeded(int amountNeeded) {
+        this.amountNeeded = amountNeeded;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public ArrayList<Medicine> getMed() {
+        return med;
+    }
+
+    public void setMed(ArrayList<Medicine> med) {
+        this.med = med;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
 }
