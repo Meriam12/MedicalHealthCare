c159d116ee6400631d7f7c1fb64b82448f3187bf
Mariam Mohamed <mariam155654@bue.edu.eg>
2021-01-13 18:12:24 +0200

    Create FacadeInterface.java

---
+++ RMIClient/src/RMI/FacadeInterface.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RMI;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ *
+ * @author Mariam
+ */
+public interface FacadeInterface {
+       public void SetMedicineData(String name, String type, String expiredDate, int amountInStock, int price);
+       public String getMedicineData();
+}
