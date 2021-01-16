66df4a169696dbea27b80cbbe0e973165b9f6db4
Mariam Mohamed <mariam155654@bue.edu.eg>
2021-01-16 02:25:27 +0200

    .

--- RMIServer/src/RMIServer/Cash.java
+++ RMIServer/src/RMIServer/Cash.java
@@ -14,14 +14,14 @@ public class Cash implements PaymentMethod{
     @Override
     public String makeAppointmentPayment(){
         int amount;
-        amount = 12;
+        amount = 100;
         return  "When you arrive to our hospital, you have to pay " + amount;
     }
     
     @Override
     public String payMedicine(){
         int amount;
-        amount = 12;
+        amount = 100;
         return  "We will come to your address after 2 days, you have to pay " + amount;
     }
     
--- RMIServer/src/RMIServer/Visa.java
+++ RMIServer/src/RMIServer/Visa.java
@@ -17,14 +17,14 @@ public class Visa implements PaymentMethod {
     @Override
     public String makeAppointmentPayment(){
         int amount;
-        amount = 12;
+        amount = 100;
         return  "We will take from your credit card " + amount + " Pounds";
     }
     
     @Override
     public String  payMedicine(){
         int amount;
-        amount = 12;
+        amount = 100;
         return  "We will take from your credit card " + amount + " Pounds";
     }
 }
